c9531982a4adca3299519d079e7032241f8268b3
Yoyae <gingin.fr@gmail.com>
2021-11-12 13:52:25 +0100

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -6,13 +6,13 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
-      port: 8545
+      port: 7545
     }
   },
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.6.11",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.9",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       settings: {          // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
